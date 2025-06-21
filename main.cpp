820e24a266157595e7de6805f74289414cad8a9e
thirofoo <83126064+thirofoo@users.noreply.github.com>
2025-06-21 14:46:17 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, n) for(int i = 0; i < n; i++)
+
+namespace utility {
+  struct timer {
+  chrono::system_clock::time_point start;
+  // 開始時間を記録
+  void CodeStart() {
+    start = chrono::system_clock::now();
+  }
+  // 経過時間 (ms) を返す
+  double elapsed() const {
+    using namespace std::chrono;
+    return (double) duration_cast<milliseconds>(system_clock::now() - start).count();
+  }
+  } mytm;
+}
+
+inline unsigned int rand_int() {
+  static unsigned int tx = 123456789, ty = 362436069, tz = 521288629, tw = 88675123;
+  unsigned int tt = (tx ^ (tx << 11));
+  tx = ty, ty = tz, tz = tw;
+  return (tw = (tw ^ (tw >> 19)) ^ (tt ^ (tt >> 8)));
+}
+
+inline double rand_double() {
+  return (double) (rand_int() % (int) 1e9) / 1e9;
+}
+
+inline double gaussian(double mean, double stddev) {
+  // 標準正規分布からの乱数生成（Box-Muller変換
+  double z0 = sqrt(-2.0 * log(rand_double())) * cos(2.0 * M_PI * rand_double());
+  // 平均と標準偏差の変換
+  return mean + z0 * stddev;
+}
+
+// 温度関数
+#define TIME_LIMIT 2950
+inline double temp(double start) {
+  double start_temp = 100, end_temp = 1;
+  return start_temp + (end_temp - start_temp) * ((utility::mytm.elapsed() - start) / TIME_LIMIT);
+}
+
+// 焼きなましの採用確率
+inline double prob(int best, int now, int start) {
+  return exp((double) (now - best) / temp(start));
+}
+
+// ビームサーチの結果復元用の構造体
+struct Trace {
+  char op;
+  int parend_id;
+  Trace(): parend_id(0) {}
+  explicit Trace(char _op, int _parend_id): op(_op), parend_id(_parend_id) {}
+};
+
+vector<Trace> traces;
+string restore(int best_id, int initial_id) {
+  string ops;
+  int current_id = best_id;
+  while(current_id != initial_id) {
+    ops += traces[current_id].op;
+    current_id = traces[current_id].parend_id;
+  }
+  reverse(ops.begin(), ops.end());
+  return ops;
+}
+
+//-----------------以下から実装部分-----------------//
+
+struct State {
+  int id;
+  long long score;
+
+  State(): score(0LL) {
+    id = generate_id();
+  }
+  explicit State(long long _score): score(_score) {
+    id = generate_id();
+  }
+
+  bool operator<(const State& other) const {
+    return score < other.score;
+  }
+  bool operator>(const State& other) const {
+    return score > other.score;
+  }
+
+  private:
+  static int generate_id() {
+    static int id_counter = 0;
+    return id_counter++;
+  }
+};
+
+struct Solver {
+  Solver() {
+    this->input();
+    
+  }
+
+  void input() {
+    return;
+  }
+
+  void output() {
+    return;
+  }
+
+  void solve() {
+    return;
+  }
+};
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+
+  Solver solver;
+  solver.solve();
+  solver.output();
+
+  return 0;
+}
